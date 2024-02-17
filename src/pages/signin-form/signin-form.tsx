20ea9dc88bf342a8cc2db40447e799792a0ce3af
vika <vika_onoshko@mail.ru>
2024-02-17 14:15:23 +0300

    fix signin page

--- src/pages/signin-form/signin-form.tsx
+++ src/pages/signin-form/signin-form.tsx
@@ -1,7 +1,6 @@
-import { Checkbox, Form, Input, Space, Typography } from 'antd';
+import { Button, Checkbox, Form, Input, Space, Typography } from 'antd';
 import { Auth } from '../../components/auth';
 import './signin-form.less';
-import { MyButton } from '@ui/button';
 import { GooglePlusOutlined } from '@ant-design/icons';
 
 export const SigninForm: React.FC = () => {
@@ -16,46 +15,51 @@ export const SigninForm: React.FC = () => {
     return (
         <Auth activeForm='signin'>
             <Form
-                className='form'
+                className='signin-form'
                 name='basic'
                 initialValues={{ remember: true }}
                 onFinish={onFinish}
                 onFinishFailed={onFinishFailed}
                 autoComplete='off'
+                size='large'
             >
-                <Space className='form__input' direction='vertical' size={32}>
-                    <Form.Item
-                        name='email'
-                        rules={[{ required: true, message: 'Please input your email!' }]}
-                    >
-                        <Input addonBefore={'e-mail:'} />
-                    </Form.Item>
+                <Form.Item
+                    className='signin-form__email'
+                    name='email'
+                    rules={[{ required: true, message: 'Please input your email!' }]}
+                >
+                    <Input addonBefore={'e-mail:'} />
+                </Form.Item>
 
-                    <Form.Item
-                        name='password'
-                        rules={[{ required: true, message: 'Please input your password!' }]}
-                    >
-                        <Input.Password placeholder='Пароль' />
-                    </Form.Item>
-                </Space>
+                <Form.Item
+                    className='signin-form__password'
+                    name='password'
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input.Password placeholder='Пароль' />
+                </Form.Item>
 
-                <Space className='form__additional' direction='horizontal'>
-                    <Form.Item className='form__check-area' name='remember' valuePropName='checked'>
+                <Space className='signin-form__additional' direction='horizontal'>
+                    <Form.Item
+                        className='signin-form__check-area'
+                        name='remember'
+                        valuePropName='checked'
+                    >
                         <Checkbox>Запомнить меня</Checkbox>
                     </Form.Item>
 
-                    <Typography.Link className='forgot-password'>Забыли пароль?</Typography.Link>
+                    <Typography.Link className='signin-form__forgot-password'>
+                        Забыли пароль?
+                    </Typography.Link>
                 </Space>
 
-                <Form.Item className='form__footer'>
-                    <MyButton type='primary' role='submit' size='large'>
-                        Войти
-                    </MyButton>
+                <Button type='primary' role='submit' className='signin-form__submit'>
+                    Войти
+                </Button>
 
-                    <MyButton type='secondary' size='large' className='google-auth'>
-                        <GooglePlusOutlined /> Войти через Google
-                    </MyButton>
-                </Form.Item>
+                <Button type='default' className='signin-form__google-auth'>
+                    <GooglePlusOutlined /> Войти через Google
+                </Button>
             </Form>
         </Auth>
     );
